c3a26d433ff8d4ba87f378f2a513860ad69f210e
hamza islah <hamza2028islah@gmail.com>
2022-02-20 23:06:51 +0100

    somme modification

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # icons form 
+
 # <a href="https://www.flaticon.com/fr/icones-gratuites/ecrivain" title="écrivain icônes">Écrivain icônes créées par Freepik - Flaticon</a>
+
+
+
 
 from ctypes import pointer
 from tkinter import *
@@ -7,20 +11,18 @@ from turtle import bgcolor
 from tools import *
 
 ###########################################
-
-
-#colorPalet={'back':'#19282F','front':'#B33030'}
 #-------window configuration -----------
+
 win=Tk() 
 win.geometry("500x500")
 win.resizable(False,False)
 win.title("TO DO")
+img = PhotoImage(file='icon.png')
+win.tk.call('wm', 'iconphoto', win._w, img)
 col=win['background']
 
 
-
-
-def addT():
+def addT(e):
     content=T.get()
     if content !="":
         Newtask=Task(Ouwrapp,content)
@@ -31,10 +33,9 @@ def addT():
 
 Inwrapp=Frame(win)
 Inwrapp.pack(fill='both')
-
-
 T = Entry(Inwrapp,width=40,font="sans-serif",borderwidth=0.5,bg="#EFEFEF")
 T.pack(side="left",padx=10,pady=10,fill=Y)
+T.bind("<Return>",addT)
 
 addTask=Button(
                 Inwrapp,text="+",
@@ -48,19 +49,22 @@ addTask=Button(
 
 addTask.pack(side='right',pady=10,padx=10,fill=Y)
 
+
 #-------------------------Output tasks frame---------------------------------
+
 canvas =Canvas(win,borderwidth=0)
 Ouwrapp = Frame(canvas)
 canvas.create_window((10, 10), window=Ouwrapp, anchor="nw")
 canvas.pack(side="left", fill="both", expand=True)
-
 scrollbar = Scrollbar(win, orient="vertical", command=canvas.yview)
 Ouwrapp.bind( "<Configure>",lambda e: canvas.configure( scrollregion=canvas.bbox("all")))
 canvas.configure(yscrollcommand=scrollbar.set)
 scrollbar.pack(side="right", fill="y")
+
 #------------------------
 
-readAllfile(Ouwrapp)
+readAllfile(Ouwrapp)   # whith open get all tasks from fils
+
 
 win.mainloop()
 win.destroy()
